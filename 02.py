3adfd44c26223cb02e1c0da9c81525500968f530
VladimirPalanzeev <palanzey@gmail.com>
2022-01-09 18:16:22 +0500

    [upd] Алгоритм поиска номера искомого элемента в спимке

---
+++ 02.py
@@ -0,0 +1,20 @@
+
+# Номер элемента
+n = "311"
+parking = ["-", "-", "132", "155", "-", "521", "311", "-", "555", "839"]
+
+park = None
+p1 = 0
+
+while park == None and p1 < len(parking):
+    if parking[p1] == n:
+        park = p1
+    p1 += 1
+
+if park != None:
+    print(f"Автомобиль стоит на {park} месте.")
+else:
+    print("Автомобиля на стоянке нет.")
+
+
+
